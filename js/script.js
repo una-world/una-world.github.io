d9e9bd98074e07c1f60ede44c561b234b41b204b
una-world <72382144+una-world@users.noreply.github.com>
2020-12-15 22:40:24 +0800

    up
    up 001

---
+++ js/script.js
@@ -0,0 +1,47 @@
+//background fadedown while scrolldown
+$(window).scroll(function() {
+    var scrollwindow = $(this).scrollTop()
+    if (scrollwindow > 1600) {
+        $("body").css({
+            animation: 'none',
+            background: '#fff',
+        })
+    } else if (scrollwindow < 1600) {
+        $("body").css({
+            animation: '',
+            background: '',
+        })
+    }
+});
+
+
+//Swiper
+var mySwiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    // direction: "vertical",
+    loop: true,
+    // If we need pagination
+    // pagination: {
+    //   el: ".swiper-pagination",
+    // },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.news_next',
+        prevEl: '.news_prev',
+    }
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: ".swiper-scrollbar",
+    // },
+});
+
+
+ 
+
+
+
+
+
+
+
+
